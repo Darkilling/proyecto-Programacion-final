b2b82e709b44654795026d50b1c57652471117fa
Darkilling <luis.canales21@inacapmail.cl>
2024-07-22 22:33:02 -0400

    probando

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
+import Link from "next/link";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      Work in progress!
-    </main>
+    <div className={`${inter.className}`}>
+      <div className="text-center font-mono text-3xl font-extrabold tracking-wider m-8">
+        Bienvenidos a mi Blog
+      </div>
+      <hr style={{ width: '100%' }} />
+      <div className="container">
+      { 
+        posts && 
+        posts.length > 0 &&
+        posts.map((post) => {
+          return (
+            <p className="m-2">
+              <li>
+                <Link href={`/posts/${post.id}`}>{ post.title }</Link>
+                &nbsp;(autor: { post.author })
+              </li>
+            </p>
+          )
+        })
+      }
+      </div>
+      <hr style={{ width: '100%' }} />
+    </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/posts");
+  const posts = await res.json();
+  return { props: { posts } };
 }
