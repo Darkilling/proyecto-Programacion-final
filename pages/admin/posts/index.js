96a62ac2772c7c1136031a68190789058342f30b
Darkilling <luis.canales21@inacapmail.cl>
2024-07-25 22:21:12 -0400

    creacion ok

--- pages/admin/posts/index.js
+++ pages/admin/posts/index.js
@@ -1,46 +1,18 @@
-import ProductForm from "../components/Productform";
-import { useState } from "react";
-import { useRouter } from "next/router";
-import { useMutation, useQueryClient } from "react-query";
-import { createProduct } from "../../../api/products";
+import React from 'react';
+import Producto from './productos';
+import Usuarios from './usuarios';
 
+const AdminPost = () => {
+  return (
+    <main>
+      <div className="container mx-auto px-4">
+        <Producto />
+        <Usuarios />
+      </div>
+    </main>
+  );
+};
 
-const CreateProduct = () => {
-    const [product, setProduct] = useState({});
-    const queryClient = useQueryClient();
-    const router = useRouter();
-    const { mutate } = useMutation(createProduct, {
-        onSuccess: () => {
-        queryClient.invalidateQueries("products");
-        router.push("/admin/products");
-        },
-    });
-    
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setProduct((prev) => ({
-        ...prev,
-        [name]: value,
-        }));
-    };
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        mutate(product);
-    };
-    
-    return (
-        <div>
-        <h1>Create Product</h1>
-        <ProductForm
-            product={product}
-            handleChange={handleChange}
-            handleSubmit={handleSubmit}
-        />
-        </div>
-    );
-    };
+export default AdminPost;
 
-
-export default CreateProduct;
 
